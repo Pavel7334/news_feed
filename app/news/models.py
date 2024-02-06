0ce3474770c9acb1f813fd14453c5b5ad08af974
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-02-06 23:52:11 +0400

    создал модель News и User

--- app/news/models.py
+++ app/news/models.py
@@ -1,2 +1,31 @@
+from datetime import datetime
+from sqlalchemy import MetaData, TIMESTAMP, Boolean, DateTime, JSON, Column, ForeignKey, Integer, String
 from app.database import Base
+
+metadata = MetaData()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+
+class News(Base):
+    __tablename__ = 'news'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author = Column(Integer, ForeignKey('user.id'))
+    summary = Column(String(250), nullable=False)
+    description = Column(String, nullable=False)
+    views = Column(Integer, default=0)
+    favourites = Column(Boolean, default=False)
+    estimation = Column(Integer, default=0)
+    rating = Column(Integer, default=0)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
